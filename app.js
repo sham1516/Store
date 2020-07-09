8b573cbfb3a6127055a7c130e98811e7facfc76a
Shamant Bhimanagoud Naik <shamant1516@gmail.com>
2020-07-09 15:55:32 -0700

    Added cart and checkout routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 // const expressHbs = require('express-handlebars');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -10,17 +11,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: '404'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,19 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const adminController = require('../controllers/admin')
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product', 
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeProduct: true
-  });
-});
+router.get('/add-product', adminController.getAddProduct);
+
+router.get('/products');
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({title: req.body.title});
-  res.redirect('/');
-});
+router.post('/add-product', adminController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+
+module.exports = router;
