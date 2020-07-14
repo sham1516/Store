a04bda3b5794ca735cf83bdc2cc3e3fe09bc0a71
Shamant Bhimanagoud Naik <shamant1516@gmail.com>
2020-07-13 19:10:30 -0700

    Editing and Delete product

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
+
 const errorController = require('./controllers/error');
 
 const app = express();
 
-// app.engine('hbs', expressHbs());
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const shopController = require('../controllers/shop')
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
@@ -16,9 +16,10 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
-
